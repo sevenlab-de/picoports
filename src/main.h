ab2ce635da0cf57ac8a481ee10dc8a86d1b2209c
Gero Schwäricke <gero.schwaericke@sevenlab.de>
2025-09-09 16:37:29 +0200

    Cleanup licensing
    The defines from the Linux kernel driver are GPL licensed and combining
    GPL with MIT is a major headache. So we're switching to Linux style GPL
    for this project (LICENSE copied from Linux kernel). I also added SPDX
    license identifiers to all source code files.

--- src/main.h
+++ src/main.h
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (c) 2025 sevenlab engineering GmbH
+ */
 #ifndef _PP_MAIN_H_
 #define _PP_MAIN_H_
 
--- src/tusb_config.h
+++ src/tusb_config.h
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (c) 2025 sevenlab engineering GmbH
+ */
 #ifndef _PICOPORTS_TUSB_CONFIG_H_
 #define _PICOPORTS_TUSB_CONFIG_H_
 
